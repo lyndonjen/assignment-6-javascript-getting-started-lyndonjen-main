315ef68b55cb9ebe2babfc27206434edf46dc966
Lyndon Jen <51458522+lyndonjen@users.noreply.github.com>
2024-10-29 08:44:21 -0700

    This is Assignment 6.

--- scripts/css-mode.js
+++ scripts/css-mode.js
@@ -1,1 +1,18 @@
 /* add your JavaScript code here! */
+
+document.addEventListener("DOMContentLoaded", function () {
+    // console.log("DOM is loaded");
+
+    function myFunction() {
+        console.log("Button has been clicked");
+        // querySelect to get the body
+        let element = document.querySelector("body");
+        // classList to get the darkmode class
+        element.classList.toggle("darkmode");
+    }
+
+    // getElementID to get button
+    let myButton = document.getElementById("btn-mode");
+    // add eventListener to see if clicked
+    myButton.addEventListener("click", myFunction);
+});
